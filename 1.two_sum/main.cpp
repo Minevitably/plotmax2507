b2f38c34e75d2fcf23ab0aaa80c12be28c9571ae
Minevitably <plotmax@163.com>
2025-07-29 20:14:33 +0800

    1.two_sum 普通的枚举解法

--- 1.two_sum/main.cpp
+++ 1.two_sum/main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+};
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
