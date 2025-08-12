a1f74b7e6ab4c39ba8f9cb33c8908469d1b6c520
Minevitably <plotmax@163.com>
2025-08-12 20:26:06 +0800

    100.same_tree 只遍历一次的递归法，相对较快

--- 100.same_tree/main.cpp
+++ 100.same_tree/main.cpp
@@ -31,58 +31,19 @@ struct TreeNode {
  * };
  */
 class Solution {
-private:
-    void inorder(TreeNode *node, vector<int> &result) {
-        if (node == nullptr) {
-            result.push_back(INT_MIN);
-            return;
-        }
-        inorder(node->left, result);
-        result.push_back(node->val);
-        inorder(node->right, result);
-    }
-
-public:
-    vector<int> inorderTraversal(TreeNode *root) {
-        vector<int> result;
-        inorder(root, result);
-        return result;
-    }
-
-private:
-    void postorder(TreeNode *node, vector<int> &result) {
-        if (node == nullptr) {
-            result.push_back(INT_MIN);
-            return;
-        }
-        postorder(node->left, result);
-        postorder(node->right, result);
-        result.push_back(node->val);
-    }
-
-public:
-    vector<int> postorderTraversal(TreeNode *root) {
-        vector<int> result;
-        postorder(root, result);
-        return result;
-    }
-
 public:
     bool isSameTree(TreeNode *p, TreeNode *q) {
-        vector<int> pInOrderList = inorderTraversal(p);
-        vector<int> pPostOrderList = postorderTraversal(p);
-        vector<int> qInOrderList = inorderTraversal(q);
-        vector<int> qPostOrderList = postorderTraversal(q);
-        if (pInOrderList.size() != qInOrderList.size()) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
+        if (p == nullptr || q == nullptr) {
             return false;
         }
-        for (int i = 0; i < pInOrderList.size(); i++) {
-            if ((pInOrderList[i] != qInOrderList[i]) ||
-                (pPostOrderList[i] != qPostOrderList[i])) {
-                return false;
-            }
+        if (p->val != q->val) {
+            return false;
         }
-        return true;
+        return isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
     }
 };
 
