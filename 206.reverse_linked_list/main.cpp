94363926a7735b709fcb720fe116b851930b57b9
Minevitably <plotmax@163.com>
2025-08-25 23:00:08 +0800

    205.isomorphic_strings 迭代法

---
+++ 206.reverse_linked_list/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution {
+public:
+    ListNode *reverseList(ListNode *head) {
+        ListNode *curr = head;
+        ListNode *prev = nullptr;
+        while (curr != nullptr) {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
