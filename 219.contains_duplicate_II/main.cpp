56811f5dd719b8b828ee9fbfae9b3199b8ce4214
Minevitably <plotmax@163.com>
2025-08-26 20:34:52 +0800

    219.contains_duplicate_II 比较傻的解法2

---
+++ 219.contains_duplicate_II/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k) {
+        map<int, int> existMap;
+        for (int i = 0; i < nums.size(); i++) {
+            int key = nums[i];
+            if (existMap.count(key) == 0) {
+                existMap[key] = i;
+            } else if (abs(existMap[key] - i) <= k) {
+                return true;
+            } else {
+                existMap[key] = i;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
